921ed78a77c7a9dfa495cb11df179c0401664ca7
Itstep <seleznev_k@itstep.org>
2019-06-19 21:06:31 +0300

    Client

--- src/main/java/org/itstep/Main.java
+++ src/main/java/org/itstep/Main.java
@@ -1,6 +1,27 @@
 package org.itstep;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Scanner keyboard = new Scanner(System.in);
+        String owner = keyboard.nextLine();
+
+        try (Socket client = new Socket("127.0.0.1", 9000)) {
+            try (
+                Scanner scanner = new Scanner(client.getInputStream());
+                PrintWriter writer = new PrintWriter(client.getOutputStream())
+            ) {
+                writer.println(0);
+                writer.println(owner);
+                writer.flush();
+
+                Integer accountNumber = Integer.parseInt(scanner.nextLine());
+                System.out.println("Создан счет: " + accountNumber);
+            }
+        }
     }
 }
